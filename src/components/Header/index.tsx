5184b35e95ae26f64886b88a0e07928e758c06fa
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-03 16:40:28 -0300

    feat: create NewTransactionModal Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,24 @@ import {
   NewTransactionsButton,
 } from "./styles";
 
+import * as Dialog from "@radix-ui/react-dialog";
+
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionsModal } from "../NewTransactionsModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="Logo Dt Money" />
-        <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+          </Dialog.DialogTrigger>
+
+          <NewTransactionsModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
