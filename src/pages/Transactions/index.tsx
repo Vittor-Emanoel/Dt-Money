dc7e076c000169e43887044568a37b0a58c12523
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-08 06:25:53 -0300

    feat: create Transactions Context and Summary func

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,7 +8,10 @@ import {
   TransactionsTable,
 } from "./styles";
 
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext);
+
   return (
     <div>
       <Header />
@@ -17,22 +21,20 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 59.999,90</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>02/01/2024</td>
-            </tr>
-            <tr>
-              <td width="50%">Lanche</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 59,90</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>01/01/2024</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
